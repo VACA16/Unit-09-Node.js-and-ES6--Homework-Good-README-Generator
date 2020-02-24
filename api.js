7da89d8468880fcf37365209febb6c0a2c67e9ed
Pastor <randyvaca11@gmail.com>
2020-02-23 22:23:40 -0600

    g

---
+++ api.js
@@ -0,0 +1,15 @@
+const axios = require("axios");
+
+// const api = {
+//     getUser(username) {
+
+axios
+    .get("https://www.omdbapi.com/?t=The%20Matrix&apikey=trilogy")
+    .then(function(res) {
+        console.log(res.data);
+    });
+
+//     }
+// };
+
+// module.exports = api;

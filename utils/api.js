444b77324945359b3f97017c7718d8be0cf10eb8
Pastor <randyvaca11@gmail.com>
2020-02-23 22:09:07 -0600

    g

--- utils/api.js
+++ utils/api.js
@@ -1,24 +1,13 @@
-const fs = require("fs");
 const axios = require("axios");
-const inquirer = require("package.json");​
 
 const api = {
     getUser(username) {
-        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;​
 
-        axios.get(queryUrl).then(function(res) {
-
-            const repoNames = res.data.map((repo) => repo.name);
-            console.log('github repos:', repoNames);
-            const repoNamesString = repoNames.join("\n");​
-
-            fs.writeFile("repos.txt", repoNamesString, function(err) {
-                if (err) {
-                    throw err;
-                }​
-                console.log(`Saved ${repoNames.length} repos`);
+        axios
+            .get("https://www.omdbapi.com/?t=The%20Matrix&apikey=trilogy")
+            .then(function(res) {
+                console.log(res.data);
             });
-        });
 
     }
 };
